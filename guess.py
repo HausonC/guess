616c749d72ca275a3b1732b544c50ed9bd1f6b0b
Hauson Chen <hauson621@hotmail.com.tw>
2024-09-12 22:30:02 +0800

    2nd ver

--- guess.py
+++ guess.py
@@ -1,10 +1,15 @@
 import random
 
-r = random.randint(1, 100)
+start = input ('please set a start no.: ')
+end = input ('please set a end no.: ')
+start = int (start)
+end = int (end)
+
+r = random.randint(start, end)
 count = 0
 while True:
 		count += 1 # count = count + 1
-		x = input ('please input a int: ')
+		x = input ('please guess a number: ')
 		x = int (x)
 		if x == r:
 			print ('you got it')
